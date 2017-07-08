c01c747175bb9330e588af80643ebd3d78073a51
Justin Li <justinhanyueli@gmail.com>
2017-07-07 19:26:05 -0500

    stuff

--- make.sh
+++ make.sh
@@ -18,5 +18,5 @@ set -e
 
 mkdir -p bin
 
-javac -Xlint $(find * | grep "\\.java$") -d ./bin -sourcepath ./src -cp ./third_party/junit4.jar:./bin
-javac -Xlint $(find * | grep "\\.java$") -d ./bin -sourcepath ./test -cp ./third_party/junit4.jar:./bin
+javac -Xlint $(find * | grep "\\.java$") -d ./bin -sourcepath ./src -cp `cygpath -w ./third_party/junit4.jar `\;`cygpath -w ./third_party/hamcrest-core.jar `
+javac -Xlint $(find * | grep "\\.java$") -d ./bin -sourcepath ./test -cp `cygpath -w ./third_party/junit4.jar `
